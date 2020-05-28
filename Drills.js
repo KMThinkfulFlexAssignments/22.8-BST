146d7f2575764e4e9441f8ab3313e2aafc244398
Kevin Moore <moore.kevin.p@gmail.com>
2020-05-28 16:13:37 -0400

    set up drills

---
+++ Drills.js
@@ -0,0 +1,29 @@
+//1. Draw a BST
+// Draw.io ()
+
+//2. Remove the root
+//Draw.io ()
+
+//3.  Create a BST class
+
+//4. What does this program do?
+/*
+Without running this code in your code editor, explain what the following program does. Show with an example the result of executing this program. What is the runtime of this algorithm?
+
+function tree(t){
+    if(!t){
+        return 0;
+    }
+    return tree(t.left) + t.value + tree(t.right)
+}
+*/
+
+//5. Height of a BST
+
+//6. Is it a BST?
+
+//7. 3rd largest node
+
+//8. Balanced BST
+
+//9. Are they the same BSTs?

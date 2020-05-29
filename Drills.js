3a48ff2f6035494f47f473b345dfc04dcc9c4af3
Kevin Moore <moore.kevin.p@gmail.com>
2020-05-29 09:34:23 -0400

    finished up through q4

--- Drills.js
+++ Drills.js
@@ -1,10 +1,45 @@
+'use strict';
+const BinarySearchTree = require('./BST');
 //1. Draw a BST
-// Draw.io ()
+// Draw.io (https://drive.google.com/file/d/1CwS1kAqP9aokuRDiOHAgMHH80zM1xRAz/view?usp=sharing)
 
 //2. Remove the root
-//Draw.io ()
+//Draw.io (https://drive.google.com/file/d/1CwS1kAqP9aokuRDiOHAgMHH80zM1xRAz/view?usp=sharing)
 
 //3.  Create a BST class
+function numberBST() {
+  let numberTree = new BinarySearchTree;
+  numberTree.insert(3, 3);
+  numberTree.insert(1, 1);
+  numberTree.insert(4, 4);
+  numberTree.insert(6, 6);
+  numberTree.insert(9, 9);
+  numberTree.insert(2, 2);
+  numberTree.insert(5, 5);
+  numberTree.insert(7, 7);
+  
+  return numberTree;
+}
+console.log(numberBST());
+
+function letterBST() {
+  let letterTree = new BinarySearchTree;
+  letterTree.insert('E', 'E');
+  letterTree.insert('A', 'A');
+  letterTree.insert('S', 'S');
+  letterTree.insert('Y', 'Y');
+  letterTree.insert('Q', 'Q');
+  letterTree.insert('U', 'U');
+  letterTree.insert('E', 'E');
+  letterTree.insert('S', 'S');
+  letterTree.insert('T', 'T');
+  letterTree.insert('I', 'I');
+  letterTree.insert('O', 'O');
+  letterTree.insert('N', 'N');
+
+  return letterTree;
+}
+console.log(letterBST());
 
 //4. What does this program do?
 /*
@@ -17,6 +52,7 @@ function tree(t){
     return tree(t.left) + t.value + tree(t.right)
 }
 */
+//Answer: This program sums the value of each node in the tree.
 
 //5. Height of a BST
 
